ceb9cfeacb3369641578cf82be4a3db5475470fb
Cleiton Amaral <Cleito_bss@hotmail.com>
2024-12-05 19:12:48 -0300

    att

--- obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f57ba8e79e59310706954c714d8bd22c4166de3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42b058230f96d5e1575528d0607bd378bc8be97")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
