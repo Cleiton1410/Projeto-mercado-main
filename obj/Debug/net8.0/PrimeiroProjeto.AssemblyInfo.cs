3e65329d1659c5b7746f08990ef594a08fbeb7c0
Cleiton Amaral <Cleito_bss@hotmail.com>
2024-12-10 20:13:21 -0300

    atualizando com eder

--- obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42b058230f96d5e1575528d0607bd378bc8be97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb9cfeacb3369641578cf82be4a3db5475470fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
